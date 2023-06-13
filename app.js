f60c8d29d92f546e55c810fbf547c80d49b8964e
Rudresh Pandey <rudreshpandey2002@gmail.com>
2023-06-13 08:29:16 +0530

    routes, packages added

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import { userLogin, userRegister } from "./controller/userController.js";
 
-
+import cors from "cors";
 const app = express();
+app.use(express.json());
+app.use(cors({
+    credentials: true,
+    origin: 'http://127.0.0.1:5173',
+}));
+
+dotenv.config();
+connectDB();
+
+app.post("/register", userRegister);
+app.post("/login", userLogin)
 
 
-app.get("/", (req, res) => {
-    res.send("home")
-})
-
-app.listen(3000, () => {
-    console.log("on port 3000")
+app.listen(process.env.PORT, () => {
+    console.log(`on port ${process.env.PORT}`)
 })
