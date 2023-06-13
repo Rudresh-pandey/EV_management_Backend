358abb362afc4a1cad694253c0114dd1d132f6ea
Rudresh Pandey <rudreshpandey2002@gmail.com>
2023-06-13 08:29:53 +0530

    User model added

---
+++ models/userModel.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        require: true,
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        require: true,
+    }
+});
+
+export default mongoose.model("User", userSchema);
